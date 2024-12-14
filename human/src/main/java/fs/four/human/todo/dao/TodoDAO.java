340c3921ced31dac0d2f8f2525ac2eea58781be9
kimhyemin <ktyy1198@naver.com>
2024-12-15 04:01:42 +0900

    todo bug fix

--- human/src/main/java/fs/four/human/todo/dao/TodoDAO.java
+++ human/src/main/java/fs/four/human/todo/dao/TodoDAO.java
@@ -13,12 +13,14 @@ import java.util.List;
 public interface TodoDAO {
 
     // 전체 Todo 조회 반환타입은 TodoVO의 리스트 ->TODO 테이블에서 조회된 여러 개의 TodoVO 객체를 반환
-    List<TodoVO> getAllTodo(TodoVO todoVO);
+    List<Todo2VO> getAllTodo(TodoVO todoVO);
     List<TodoStageCountVO> getTodoStageCount(String dept);
     int addTodo(Todo2VO todoVO);
     int addTodoDetail(TodoDetailVO todoDetailVO);
 
     // Todo 업데이트
     int updateTodo(TodoVO todoVO);
+
+    int updateTodo2(Todo2VO todoVO);
 
 }
